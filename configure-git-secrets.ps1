f9e316bb88062ca6f07d1b2de7c23c763c51aa86
maddy00o7 <149822847+maddy00o7@users.noreply.github.com>
2023-12-14 14:45:47 +0530

    setting up envireomnet

---
+++ configure-git-secrets.ps1
@@ -0,0 +1,33 @@
+# Install git-secrets
+git secrets --install
+
+# Register AWS patterns
+git secrets --register-aws
+
+# Add patterns for sensitive information
+$patterns = @(
+    'password = ',
+    'api_key = ',
+    'secret_key = ',
+    'access_key = ',
+    'auth_token = ',
+    'oauth_token = ',
+    'bearer_token = ',
+    'AWS_ACCESS_KEY_ID',
+    'AWS_SECRET_ACCESS_KEY',
+    'GITHUB_TOKEN',
+    'SLACK_TOKEN',
+    'DB_PASSWORD',
+    'DB_USERNAME',
+    'DB_CONNECTION_STRING',
+    'connectionString = ',
+    '-----BEGIN PRIVATE KEY-----',
+    '-----BEGIN PUBLIC KEY-----'
+)
+
+foreach ($pattern in $patterns) {
+    git secrets --add $pattern
+}
+
+# Verify the patterns
+git secrets --list
